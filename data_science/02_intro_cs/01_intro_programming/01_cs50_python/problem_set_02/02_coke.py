3723dd8bfad846029b64613951f16f15b50c7ffe
EauCoding <eaucoding@gmail.com>
2025-09-13 18:35:47 -0700

    Added problem set 2 with problem 2 for CS50 python

---
+++ data_science/02_intro_cs/01_intro_programming/01_cs50_python/problem_set_02/02_coke.py
@@ -0,0 +1,9 @@
+amount = 50
+
+while amount > 0:
+    print(f"Amount Due: {amount}")
+    coin = int(input("Insert Coin: "))
+    if coin == 25 or coin == 10 or coin == 5:
+        amount -= coin
+
+print(f"Change Owed: {amount * (-1)}")
