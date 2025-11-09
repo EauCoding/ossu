a5bb8c5932f340868a89d7d4cf427e492546d009
EauCoding <eaucoding@gmail.com>
2025-11-09 00:00:31 -0800

    Added problem set 0 with problem 2 for CS50 sql

---
+++ data_science/02_intro_cs/01_intro_programming/02_cs50_sql/problem_set_00/02_views/04.sql
@@ -0,0 +1,8 @@
+-- Write a SQL query to count how many prints by Hiroshige have English titles that refer to the “Eastern Capital”.
+-- Hiroshige’s prints were created in Japan’s “Edo period,” referencing the eastern capital city of Edo, now Tokyo.
+SELECT COUNT(*) AS 'number of prints'
+FROM "views"
+WHERE 
+    "artist" = 'Hiroshige' 
+    AND "english_title" LIKE '%Eastern Capital%'
+;
---
+++ data_science/02_intro_cs/01_intro_programming/02_cs50_sql/problem_set_00/02_views/08.sql
@@ -0,0 +1,9 @@
+-- Write a SQL query to list the English titles of the 5 prints with the least contrast by Hokusai, from least to highest contrast. 
+-- Compare them to this list on Wikipedia to see if your results match the print’s aesthetics.
+-- https://en.wikipedia.org/wiki/Thirty-six_Views_of_Mount_Fuji
+SELECT "english_title"
+FROM "views"
+WHERE "artist" = 'Hokusai'
+ORDER BY "contrast"
+LIMIT 5
+;
